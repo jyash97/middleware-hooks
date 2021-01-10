9a16d517fff3ac8ab50e29ad8b2ec98c68e26473
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-01-11 04:39:30 +0530

    feat: add type check for thunk

--- src/useThunk.ts
+++ src/useThunk.ts
@@ -1,13 +1,24 @@
 import { useReducer } from 'react';
 
-export const useThunk = (reducer: any, initialState: any) => {
+export const useThunk = <T>(
+  reducer: (
+    stateArg: T,
+    actionArg: {
+      type: string;
+    }
+  ) => T,
+  initialState: T
+) => {
   const [store, dispatch] = useReducer(reducer, initialState);
 
-  const enhancedDispatch = (action: any) => {
+  const enhancedDispatch = (
+    action:
+      | { type: string }
+      | ((dispatchArg: React.Dispatch<{ type: string }>) => void)
+  ) => {
     if (typeof action === 'function') {
       action(dispatch);
     } else {
-      // @ts-ignore
       dispatch(action);
     }
   };
