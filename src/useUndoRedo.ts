74ec29dc6b5c66673fa8b38975d2f38cb6aba312
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-01-11 00:27:02 +0530

    feat: add generic types for internal state

--- src/useUndoRedo.ts
+++ src/useUndoRedo.ts
@@ -1,18 +1,28 @@
 import { useReducer } from 'react';
 
-export const ACTION_TYPES = {
-  REDO: 'REDO',
-  UNDO: 'UNDO',
-};
+export enum ACTION_TYPES {
+  REDO = 'REDO',
+  UNDO = 'UNDO',
+}
 
-export const useUndoRedo = (reducer: Function, initialState: any) => {
-  const _internalState = {
+interface InternalState<T> {
+  past: T[];
+  present: T;
+  future: T[];
+}
+
+interface InternalActionType {
+  type: ACTION_TYPES;
+}
+
+export const useUndoRedo = <T>(reducer: Function, initialState: T) => {
+  const _internalState: InternalState<T> = {
     past: [],
     present: initialState,
     future: [],
   };
 
-  const _reducer = (state: any, action: any) => {
+  const _reducer = (state: InternalState<T>, action: InternalActionType) => {
     switch (action.type) {
       case ACTION_TYPES.UNDO: {
         const [newPresent, ...past] = state.past;
